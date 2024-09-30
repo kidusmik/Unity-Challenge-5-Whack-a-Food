7d3d7de4f22c29888ea2b1a46e9b47deac357de3
kidusmik <kidusmik@gmail.com>
2024-09-30 14:58:03 +0300

    first commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    private float spawnRate = 1.0f;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public Button restartButton;
+    public bool isGameActive;
+
+    private int score;
+    public GameObject titleScreen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // StartGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (isGameActive)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        isGameActive = false;
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void StartGame(int difficulty)
+    {
+        isGameActive = true;
+        score = 0;
+        UpdateScore(0);
+        spawnRate /= difficulty;
+        StartCoroutine(SpawnTarget());
+        titleScreen.gameObject.SetActive(false);
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Unity Challenge 5: Whack a Food
+
+The __Challenge 5__ files are located inside the __Assets > Challenge 5__ folder
+
+The __Assets > Course Library__ folder is the folder that contains __Exercise 5__ files which is basically introduction and tutorial.
+
+_Have Fun!_
